5b8182caf6084092fb2656592ee1bd1bd60aee2b
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2023-02-13 22:42:12 -0300

    ultimos ajustes para verificação e possível entrega

--- script.js
+++ script.js
@@ -1,56 +1,92 @@
-//botão randomico - sem o incluir o index 0 do black//
-const section = document.querySelector("section");
 
-const randonButton = document.createElement("button");
-section.appendChild (randonButton);
-randonButton.id = "button-random-color";
-randonButton.innerHTML = "Cores aleatórias"
-
-const colorspalette = document.querySelectorAll(".color");
-
-const randomColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-  
-randonButton.addEventListener('click', () => {
-    for (let index = 1; index < colorspalette.length; index += 1) {
-      colorspalette[index].style.backgroundColor = randomColor();
-    }
-  });
+const blackColor = document.getElementById('black');
+blackColor.style.backgroundColor = 'black';
+blackColor.className += ' selected';
 
 
+const greenColor = document.getElementById('green');
+greenColor.style.backgroundColor = 'green';
 
-// //local storage-------------!!!!!!!!!!!!????????//
+const yellowColor = document.getElementById('yellow');
+yellowColor.style.backgroundColor = 'yellow';
 
-// function saveLocalStorage(){
-//     const newPaletteColors = document.querySelector("#color-palette");
-//     localStorage.setItem("newColors",colorspalette);
-// }
+const blueColor = document.getElementById('blue');
+blueColor.style.backgroundColor = 'blue';
 
 
-//quadro de pixels//
-
-const pixelBoard = document.getElementById('pixel-board');
-
-for (let index = 0; index < 5; index += 1) {
-    const line = document.createElement("div");
-    line.style.display = 'flex';
-                for (let i = 0; i < 5; i += 1) {
-                    const pixel = document.createElement("div")
-                    pixel.classList.add("pixel");
-                    line.appendChild(pixel);
+const selectColor = (event) => {
+  const colorClicked = event.target;
+  const selected = document.getElementsByClassName('selected')[0];
+  selected.classList.remove('selected');
+  colorClicked.className += ' selected';
+};
+const verifyClickPalette = () => {
+  const colors = document.getElementById('color-palette').children;
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].addEventListener('click', selectColor);
   }
-  pixelBoard.appendChild(line);
-}
+};
+verifyClickPalette();
 
-const createPixels = (scale) => {
+
+const hexColor = () => {
+  const letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let hexColorCode = '#';
+
+  for (let i2 = 0; i2 < 6; i2 += 1) {
+    hexColorCode += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return hexColorCode;
+};
+const randomizeColors = () => {
+  const saveColors = [];
+  const colors = document.getElementsByClassName('color');
+  for (let i = 1; i < colors.length; i += 1) {
+    const color = colors[i];
+    color.style.backgroundColor = hexColor();
+    saveColors.push(color.style.backgroundColor);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(saveColors));
+};
+const btnRandomColor = document.getElementById('button-random-color');
+btnRandomColor.addEventListener('click', randomizeColors);
+const clearBoard = () => {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i];
+    pixel.style.backgroundColor = 'white';
+  }
+};
+const btnCleanColors = document.getElementById('clear-board');
+btnCleanColors.addEventListener('click', clearBoard);
+
+// // Cria função que salva as cores da paleta no localStorage
+// const loadColors = () => {
+//   if (localStorage.getItem('colorPalette') !== null) {
+//     const colors = document.getElementsByClassName('color');
+//     const returnedColors = JSON.parse(localStorage.getItem('colorPalette'));
+//     for (let i = 1; i < colors.length; i += 1) {
+//       const color = returnedColors[i - 1];
+//       colors[i].style.backgroundColor = color;
+//     }
+//   }
+// };
+
+
+const createLines = (size) => {
+  const sectionPixels = document.getElementById('pixel-board');
+  for (let i = 1; i <= size; i += 1) {
+    const linePixels = document.createElement('ul');
+    linePixels.className = 'line';
+    linePixels.style.listStyle = 'none';
+    sectionPixels.appendChild(linePixels);
+  }
+};
+const createPixels = (size) => {
   const lines = document.getElementsByClassName('line');
-  for (let index = 0; index < lines.length; index += 1) {
-    const line = lines[index];
-    for (let index2 = 1; index2 <= scale; index2 += 1) {
+  for (let i = 0; i < lines.length; i += 1) {
+    const line = lines[i];
+    for (let i2 = 1; i2 <= size; i2 += 1) {
       const pixels = document.createElement('li');
       pixels.className = 'pixel';
       pixels.style.backgroundColor = 'white';
@@ -58,17 +94,38 @@ const createPixels = (scale) => {
     }
   }
 };
-
-const createBoard = (scale) => {
-    // createLines(scale);
-    createPixels(scale);
-  };
+const createBoard = (size) => {
+  createLines(size);
+  createPixels(size);
+};
 createBoard(5);
 
-// Defina a cor preta como cor inicial da paleta de cores
 
-// Colorindo os pixels
+const savePixelArt = () => {
+  const pixels = document.getElementsByClassName('pixel');
+  const colorPixels = [''];
+  for (let i = 0; i < pixels.length; i += 1) {
+    const colorSinglePixel = pixels[i].style.backgroundColor;
+    colorPixels[i] = colorSinglePixel;
+  }
+  localStorage.setItem('pixelBoard', JSON.stringify(colorPixels));
+};
 
-// function setPixelColour{
-//     pixel.style.backgroundColor = "black";
-// }
+const paintPixel = (event) => {
+  const pixelClicked = event.target;
+  const colorSelected = document.getElementsByClassName('selected')[0].style.backgroundColor;
+  pixelClicked.style.backgroundColor = colorSelected;
+  savePixelArt();
+};
+const verifyClickPixel = () => {
+  const lines = document.getElementsByClassName('line');
+  for (let i = 0; i < lines.length; i += 1) {
+    const pixels = lines[i].children;
+    for (let i2 = 0; i2 < pixels.length; i2 += 1) {
+      const pixel = pixels[i2];
+      pixel.addEventListener('click', paintPixel);
+    }
+  }
+};
+verifyClickPixel();
+
